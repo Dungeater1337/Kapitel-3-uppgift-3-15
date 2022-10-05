ab4617268674dd3984685c9038e524f8be9a5617
Admin <Admin@TEB123021>
2022-10-05 11:50:14 +0200

    Add project files.

---
+++ Kapitel 3 uppgift 3,15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System; 
+
+namespace uppgift3_15
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen?");
+            int svar = int.Parse(Console.ReadLine());
+
+            int avgift = 80; 
+            
+            int pris = svar * avgift;
+
+            if (svar > 1 && svar < 11)
+            {
+                Console.WriteLine("det kommer att kosta dig "  +  pris); 
+                  
+            }
+
+            else if (svar > 11)
+            {
+                Console.WriteLine("NEJJJJ NEJJJJJ"); 
+            }
+
+
+
+
+
+        }
+
+
+    }
+    
+}
